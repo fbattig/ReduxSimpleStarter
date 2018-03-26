589de90b6c3bcfc1fe8b582592f772f6462d47af
Felix <fbattig@yahoo.com>
2018-03-26 09:39:34 -0400

    reduce/container

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function () {
+  [
+    { title: 'Javascript: the good part' },
+    { title: 'React and Redux' },
+    { title: 'Asp.NET Code' },
+    { title: 'Angular5' },
+  ]
+
+}
